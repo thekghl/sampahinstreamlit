cfa2137cefe079355157288ec5f922c3437da4ed
CapDMarshal <fardilabintang476@gmail.com>
2025-05-31 22:37:04 +0700

    update transaksi

--- dashboard.py
+++ dashboard.py
@@ -5,4 +5,4 @@ st.logo("data/geming.png", size="large")
 st.title("Dashboard Sampahin")
 
 st.metric("Total Pendapatan", 10000, delta=None, delta_color="normal", help=None, label_visibility="visible", border=True)
-st.metric("Total Transaksi", 10000, delta=None, delta_color="inverse", help=None, label_visibility="visible", border=True)
+st.metric("Total Transaksi", 50000, delta=None, delta_color="inverse", help=None, label_visibility="visible", border=True)

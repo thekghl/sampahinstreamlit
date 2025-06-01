5514283a044db4e84a4916df011ce79314307714
thekghl <vintdm.vt@gmail.com>
2025-06-01 23:09:19 +0700

    selesai coy lesgo

--- users_pages/user_katalog.py
+++ users_pages/user_katalog.py
@@ -9,14 +9,14 @@ def show():
         jenis_sampah = st.selectbox("Jenis Sampah", ["logam", "plastik", "karet", "kertas"])
         total_sampah = st.number_input("Total Sampah (kg)", min_value=0.0, step=0.1)
         if jenis_sampah == "logam":
-            total_sampah = total_sampah * 2160
+            total_harga = total_sampah * 2160
         elif jenis_sampah == "plastik":
-            total_sampah = total_sampah * 50
+            total_harga = total_sampah * 50
         elif jenis_sampah == "karet":
-            total_sampah = total_sampah * 1400
+            total_harga = total_sampah * 1400
         elif jenis_sampah == "kertas":
-            total_sampah = total_sampah * 3000
-        formatted_total = f"{total_sampah:,}".replace(",", ".")
+            total_harga = total_sampah * 3000
+        formatted_total = f"{total_harga:,}".replace(",", ".")
         st.write(f"Total Harga Sampah: Rp. {formatted_total}")
         
         submitted = st.form_submit_button("Kirim Laporan")
@@ -27,9 +27,10 @@ def show():
                 user_id = st.session_state.get("user", {}).get("id_user")
                 today = date.today()
                 status = "proses"
+                id_admin = 1
                 cursor.execute(
-                    "INSERT INTO transaksi_sampah (id_user, jenis_sampah, total_sampah, tanggal, status) VALUES (%s, %s)",
-                    (user_id, jenis_sampah, total_sampah, today, status)
+                    "INSERT INTO transaksi_sampah (id_admin, id_user, jenis_sampah, total_sampah, tanggal, status, total_pembayaran) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                    (id_admin, user_id, jenis_sampah, total_sampah, today, status, total_harga)
                 )
                 conn.commit()
                 cursor.close()
